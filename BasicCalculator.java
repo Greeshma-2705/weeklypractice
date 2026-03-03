bcce58dcc0167c2cc152b21e8079404ad98976d7
sg4675 <sg4675@srmist.edu.in>
2026-03-03 10:29:03 +0530

    Update BasicCalculator.java

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -10,9 +10,7 @@ public class BasicCalculator {
         System.out.print("Enter second number: ");
         double num2 = input.nextDouble();
 
-        System.out.println("Addition = " + (num1 + num2));
-        System.out.println("Subtraction = " + (num1 - num2));
-        System.out.println("Multiplication = " + (num1 * num2));
-        System.out.println("Division = " + (num1 / num2));
+        System.out.println("Addition = " + (num1 + num2)+" ,"+"Subtraction = " + (num1 - num2)+","+"Multiplication = " + (num1 * num2)+","+"Division = " + (num1 / num2));
     }
+
 }
