ef8a9b8655c1b81b76d9cdb22610c78be537d91c
greeshma-2705 <greshmasalla@gmail.com>
2026-04-01 20:22:57 +0530

    initial commit

---
+++ Friends.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Friends {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] age = new int[3];
+        int[] height = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Enter age and height:");
+            age[i] = sc.nextInt();
+            height[i] = sc.nextInt();
+        }
+
+        int youngest = age[0];
+        int tallest = height[0];
+
+        for (int i = 1; i < 3; i++) {
+            if (age[i] < youngest)
+                youngest = age[i];
+
+            if (height[i] > tallest)
+                tallest = height[i];
+        }
+
+        System.out.println("Youngest Age: " + youngest);
+        System.out.println("Tallest Height: " + tallest);
+    }
+}
